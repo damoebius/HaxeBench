429e4a3a8a309a4d913fc3b1397b508a68a22cd3
MOUTON <damo@tamina-online.com>
2018-05-24 15:15:03 +0200

    add haxe

---
+++ src/haxe/Main.hx
@@ -0,0 +1,78 @@
+﻿package haxe;
+
+import js.html.XMLHttpRequestResponseType;
+import js.Browser;
+import js.html.Document;
+import js.html.InputElement;
+import js.html.Int32Array;
+import js.html.XMLHttpRequest;
+import js.html.ArrayBuffer;
+import js.html.Uint8Array;
+import js.html.CanvasElement;
+import haxe.Timer;
+import haxe.ScreenPressor;
+
+class Main 
+{
+	static inline var X = 960;
+	static inline var Y = 540;
+	
+	static function main() 
+	{
+		var txt = js.Browser.document.getElementById("txt");
+		txt.innerHTML = "sss";
+		var r = new XMLHttpRequest();		
+		r.onload = function(x : Dynamic) { 
+			var buf : ArrayBuffer = r.response;
+			txt.innerHTML = "received " + buf.byteLength;
+			var btn = Browser.document.getElementById("btn");
+			btn.onclick = function(e) {	
+				var bytes = new Uint8Array(buf, 4);			
+				var sp = new ScreenPressor(X, Y);
+				var dst = new Int32Array(X * Y);
+				var inp : InputElement = cast(Browser.document.getElementById("N"), InputElement);
+				var N = Std.parseInt( inp.value );				
+				if (N < 0 || N > 1000) {
+					txt.innerHTML = "You must be joking!"; return;
+				}
+				txt.innerHTML = "Decompressing " + N + " times...";
+				clearImg();
+				Timer.delay( function() {
+					var t0 = Timer.stamp();
+					for(n in 0...N)
+						sp.DecompressI(bytes, dst);    
+					var t1 = Timer.stamp();
+					txt.innerHTML += " t=" + (t1 - t0);
+					showImg(dst);
+				}, 20);
+			}
+		};
+		r.open("GET", "blow.spi", true);
+		r.responseType = XMLHttpRequestResponseType.ARRAYBUFFER;
+		r.send();
+	}
+	
+	static function clearImg():Void
+	{
+		var m = js.Browser.document.getElementById("main");
+		while (m.lastChild != null) m.removeChild(m.lastChild);		
+	}
+	
+	static function showImg(pic:Int32Array):Void 
+	{
+		var m = js.Browser.document.getElementById("main");
+		var c = Browser.document.createCanvasElement();
+		c.width = X; c.height = Y;
+		m.appendChild(c);
+		var imagedata = c.getContext2d().getImageData(0, 0, X, Y);
+		var data = imagedata.data;
+		var picbytes = new Uint8Array(pic.buffer);
+		for(i in 0...X*Y) {
+			data[i*4+0] = picbytes[i*4+0];
+			data[i*4+1] = picbytes[i*4+1];
+			data[i*4+2] = picbytes[i*4+2];
+			data[i*4+3] = 255;
+		}
+		c.getContext2d().putImageData(imagedata, 0, 0);
+	}
+}
---
+++ src/haxe/RangeCoder.hx
@@ -0,0 +1,132 @@
+package haxe;
+
+import js.html.Uint8Array;
+import js.html.Uint32Array;
+
+class RangeCoder 
+{
+	var range : Int;
+	var code : Int;
+	var data : Uint8Array;
+	var pos : Int;
+		
+	static inline var TOP = 0x01000000;
+	static inline var BOT =   0x010000;
+		
+	public function new() 
+	{		
+	}
+
+	public function DecodeBegin(src : Uint8Array, pos0 : Int):Void
+	{
+		code = 0;
+		//range = 4294967295;// 0xFFFFFFFF;		
+		var ff = 0xFFFF;
+		range = ff * 65536;
+		range += ff;
+		data = src;
+		pos = pos0;
+		code = 0;
+		code = (code * 256) + data[pos + 1];
+		code = (code * 256) + data[pos + 2];
+		code = (code * 256) + data[pos + 3];
+		code = (code * 256) + data[pos + 4];
+		pos += 5;
+	}
+	
+	inline function decode(cumFreq : Int, freq:Int, total_freq:Int) :Void
+	{
+		code -= cumFreq * range;
+		range = range * freq;
+		while ( range < TOP ) { 
+			code = (code * 256) + data[pos++];  range *= 256; 
+		}
+	}
+	
+ 	inline function get_freq(total_freq:Int):Int 
+	{
+		range = Std.int(range / total_freq);
+		return Std.int(code / range);
+	}
+		
+	public inline function DecodeVal(cnt:Uint32Array, maxc:Int, step:Int):Int 
+	{
+		var totfr = cnt[maxc];
+		var value = get_freq(totfr);
+		var c = 0;
+		var cumfr = 0;
+		var cnt_c = 0;
+		while (c < maxc) {	
+			cnt_c = cnt[c];
+			if (value >= cumfr + cnt_c)	
+				cumfr += cnt_c;	
+			else
+				break;
+			c++;
+		}	
+		decode(cumfr, cnt_c, totfr);
+	
+		cnt[c] = cnt_c + step;
+		totfr += step;		
+		if (totfr > BOT) {
+			totfr = 0;
+			for (i in 0...maxc) {
+				var nc = (cnt[i] >> 1) + 1;
+				cnt[i] = nc;///	cnt[i] = (cnt[i]>>1)+1;
+				totfr += nc;
+			}			
+		}
+		cnt[maxc] = totfr;
+		return c;
+	}
+	
+	public inline function DecodeValUni(cnt:Uint32Array, off:Int, step:Int):Int
+	{
+		var totfr = cnt[off + 16];
+		var value = get_freq(totfr);
+
+		var x = 0;
+		var cumfr = 0;
+		var cnt_x = 0;
+		while (x < 16) {
+			cnt_x = cnt[off + x];
+			if (value >= cumfr + cnt_x)
+				cumfr += cnt_x;
+			else
+				break;
+			x++;
+		}
+
+		var c = x * 16;
+		var cnt_c = 0;
+		while (c < 256) {
+			cnt_c = cnt[off + c + 17];
+			if (value >= cumfr + cnt_c)
+				cumfr += cnt_c;
+			else
+				break;
+			c++;
+		}
+		decode(cumfr, cnt_c, totfr);
+		cnt[off + c + 17] = cnt_c + step; ///	cnt[c] += step;
+		cnt[off + x] = cnt_x + step; ///	cnt[256+x] += step;
+		totfr += step;
+		if (totfr > BOT) {
+			totfr = 0;
+			for (i in (off+17)...(off+256+17)) {
+				var nc = (cnt[i] >> 1) + 1;
+				cnt[i] = nc; ///cnt[i] = (cnt[i]>>1)+1;
+				totfr += nc;
+			}
+			for(i in 0...16) {
+				var sum = 0;
+				var i16_17 = off + (i << 4) + 17;
+				for(j in 0...16)
+					sum += cnt[i16_17 + j];///cnt[256+i] += cnt[i*16+j];
+				cnt[off + i] = sum;
+			}
+		}
+		cnt[off + 16] = totfr;
+		return c;
+	}
+}
