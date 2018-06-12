69d9741e7f5fdd232bf320484d9a6b143bc94490
damoebius <tintin33$>
2018-06-12 07:57:37 +0200

    add hxcpp

---
+++ www/js/haxecpp.js/HxcppConfig.h
@@ -0,0 +1,26 @@
+#ifndef HXCPP_CONFIG_INCLUDED
+#define HXCPP_CONFIG_INCLUDED
+
+#if !defined(HX_LINUX) && !defined(NO_HX_LINUX)
+#define HX_LINUX 
+#endif
+
+#if !defined(HXCPP_M64) && !defined(NO_HXCPP_M64)
+#define HXCPP_M64 
+#endif
+
+#if !defined(HXCPP_VISIT_ALLOCS) && !defined(NO_HXCPP_VISIT_ALLOCS)
+#define HXCPP_VISIT_ALLOCS 
+#endif
+
+#if !defined(HXCPP_API_LEVEL) && !defined(NO_HXCPP_API_LEVEL)
+#define HXCPP_API_LEVEL 331
+#endif
+
+#if !defined(HXCPP_M64) && !defined(NO_HXCPP_M64)
+#define HXCPP_M64 
+#endif
+
+#include <hxcpp.h>
+
+#endif
---
+++ www/js/haxecpp.js/mk.sh
@@ -0,0 +1,1 @@
+em++ --bind -o code.html -s WASM=1 -O3 hxcpp/Main.cpp haxe/io/Bytes.cpp haxe/io/ArrayBufferViewImpl.cpp  -s TOTAL_MEMORY=33554432
