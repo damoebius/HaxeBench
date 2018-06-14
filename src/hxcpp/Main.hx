0493945652966fe28e93813ffef2322326f98551
MOUTON <damo@tamina-online.com>
2018-06-14 17:52:00 +0200

    add hxcpp 4

--- src/hxcpp/Main.hx
+++ src/hxcpp/Main.hx
@@ -4,20 +4,23 @@ import hxcpp.ScreenPressor;
 
 @:buildXml("
 <linker id='exe' exe='emcc'>
+    <flag value='--bind' />
+    <flag value='-s' />
+    <flag value='TOTAL_MEMORY=33554432' />
 	<flag value='-s' />
 	<flag value='WASM=1' />
 </linker>
 ")
-class Main 
+class Main
 {
 	static inline var X = 960;
 	static inline var Y = 540;
 	static var ScreenPressor;
-	
-	static function main() 
+
+	static function main()
 	{
 		ScreenPressor = new ScreenPressor(X,Y);
 		trace('hello');
 	}
-	
+
 }
